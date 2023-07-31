6678940145e639aa7fc7990b5db4b91a978b0d5f
yoohmoon <ansekfzhd5455@naver.com>
2023-07-31 19:24:13 +0900

    Modify: 모달창 등장 애니메이션 추가

--- src/components/Main/components/Modal.tsx
+++ src/components/Main/components/Modal.tsx
@@ -4,7 +4,9 @@ import { styled } from 'styled-components';
 import loginModal from '../../../store/loginModal';
 
 const Modal = () => {
-  // ➕ 아래에서 위로 등장하는 애니메이션 효과 추가
+  // ➕ 모달 창 오픈 시, 배경 스크롤 막기 기능 추가
+
+  // ➕ 모달 내부 클릭 시, 모달 닫히는 버그 수정
 
   const [openLoginModal, setOpenLoginModal] = useRecoilState(loginModal);
   const closeModal = () => {
@@ -41,8 +43,24 @@ const Container = styled.div<{ modalState: boolean }>`
 const ModalContainer = styled.div<{ modalState: boolean }>`
   display: ${(props) => (props.modalState ? 'block' : 'none')};
   width: 570px;
+  /*  */
+  height: 600px;
   background-color: #fff;
   border-radius: 12px;
+
+  // ✔ 아래에서 위로 등장하는 애니메이션 효과 추가
+  animation: showModal 0.4s;
+
+  @keyframes showModal {
+    0% {
+      visibility: hidden;
+      transform: translateY(100%);
+    }
+    100% {
+      visibility: visible;
+      transform: translateY(0);
+    }
+  }
 `;
 
 const ModalHeader = styled.header`
