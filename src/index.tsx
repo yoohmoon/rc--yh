41245fe21b2fc7bfd71a7c8c6c4cdaac62999263
yoohmoon <ansekfzhd5455@naver.com>
2023-08-18 19:39:00 +0900

    Delete : 복제 파일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -4,34 +4,9 @@ import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <TodoList /> },
-//   { path: '/main/:id', element: <MainSection /> },
-//   // { path: '/c', element: <Class /> },
-// ]);
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-// function RootComponent() {
-//   const locale = useRecoilValue(localeState);
-//   const messages = { 'en-US': enUsMsg, ko: koMsg }[locale];
-
-//   return (
-//     <Router>
-//       <GlobalStyle />
-//       <IntlProvider locale={locale} messages={messages}>
-//         <ThemeProvider theme={theme}>
-//           <Routes>
-//             <Route path='/' element={<Main />} />
-//             <Route path='/rooms/:roomId' element={<Rooms />} />
-//           </Routes>
-//         </ThemeProvider>
-//       </IntlProvider>
-//     </Router>
-//   );
-// }
 
 root.render(
   <RecoilRoot>
